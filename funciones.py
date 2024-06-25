7309ba285f194d287a07fdd0700a38f49302e448
Marcelo Aquino <Marceloaquino1676@gmail.com>
2024-06-25 11:23:03 -0300

    1.1

--- funciones.py
+++ funciones.py
@@ -1,14 +1,13 @@
 import pygame
+
 def precionar_boton(coordenadas_x: tuple, coordenadas_y: tuple, boton: str, raton_x: int, raton_y: int) -> bool:
    
     mensaje = False 
     sonido_click = pygame.mixer.Sound("sonido\click.wav")
     sonido_click.set_volume(0.35)
     sonido_click.play()
-    if coordenadas_x[0] <= raton_x <= coordenadas_x[1] and coordenadas_y[0] <= raton_y <= coordenadas_y[1]:
-        
+    if coordenadas_x[0] <= raton_x <= coordenadas_x[1] and coordenadas_y[0] <= raton_y <= coordenadas_y[1]: 
         if boton == "JUGAR":
-            mensaje = True
-           
+            mensaje = True      
     return mensaje
 
--- pantalla.py
+++ pantalla.py
@@ -38,8 +38,8 @@ fondo_preguntas = pygame.image.load(r"imagenes/fondo1_preguntas.png")
 fondo_preguntas = pygame.transform.scale(fondo_preguntas, DIMENCIONES)
 
 # Coordenadas de botones
-BOTON_PLAY_X = (300, 550)
-BOTON_PLAY_Y = (480, 600)
+BOTON_PLAY_X = (310, 550)
+BOTON_PLAY_Y = (480, 606)
 x_ventana_izquierda = 0
 x_ventana_derecha = ANCHO // 2
 
@@ -54,51 +54,55 @@ while bandera == True:  # bucle infinito para que se repita la pantalla
     ventana.fill(colores.NEGRO)  # relleno de un color la pantalla
     lista_eventos = pygame.event.get()
     for evento in lista_eventos:
-        print(evento)
         if evento.type == pygame.QUIT:  # pregunto si se presionó la X de la ventana
             bandera = False
         elif evento.type == pygame.MOUSEBUTTONDOWN:
+                #evento.button = boton mouse izquierdo
             if evento.button == 1 and bandera_jugar == False:  
                 raton_x, raton_y = evento.pos
                 jugar = precionar_boton(BOTON_PLAY_X, BOTON_PLAY_Y, "JUGAR", raton_x, raton_y)
+                
             if jugar == True:
                 bandera_jugar = True
                 mostrar_botones = False
-                tiempo_inicializado = False  # Asegúrate de reiniciar la bandera del tiempo inicial
-
+                
+                
+    ventana.blit(fondo_cortina_izquierda, (x_ventana_izquierda, 0))
+    ventana.blit(fondo_cortina_derecha, (x_ventana_derecha, 0))
+    if mostrar_botones == True:
+        ventana.blit(fondo_botones, (10, 460))
     if jugar == True:
-        if x_ventana_izquierda > -ANCHO // 2 and x_ventana_derecha < ANCHO:
+        if x_ventana_izquierda > - 600:
             x_ventana_izquierda -= VELOCIDAD
             x_ventana_derecha += VELOCIDAD
         elif tiempo_inicializado == False:
             tiempo_inicial = pygame.time.get_ticks()  # Inicializar el tiempo cuando las cortinas se han movido completamente
             tiempo_inicializado = True
 
-    ventana.blit(fondo_cortina_izquierda, (x_ventana_izquierda, 0))
-    ventana.blit(fondo_cortina_derecha, (x_ventana_derecha, 0))
 
-    if mostrar_botones == True:
-        ventana.blit(fondo_botones, (10, 460))
-    if mostrar_botones == False and x_ventana_derecha >= ANCHO:
+    if x_ventana_derecha >= ANCHO:
         # Limpiar la pantalla y dibujar el fondo
         ventana.blit(fondo_preguntas, (0, 0))
-        
         # Calcular el tiempo transcurrido y restante
         tiempo_actual = pygame.time.get_ticks()
         tiempo_transcurrido = tiempo_actual - tiempo_inicial
         tiempo_restante = ultimo_tiempo - int(tiempo_transcurrido / 1000)
         if tiempo_restante < 0:
-            tiempo_restante = 0
-        
-        # Dibujar el tiempo restante en la pantalla
+            tiempo_restante = 0 
         if tiempo_restante > 20:
-            temporizador = fuente_reloj.render(f"{tiempo_restante}", True, colores.NEGRO)
+            color = colores.NEGRO
         elif tiempo_restante > 10:
-            temporizador = fuente_reloj.render(f"{tiempo_restante}", True, colores.AMARILLO)
+            color = colores.NARANJA
+        elif tiempo_restante > 0:
+            color = colores.ROJO
         else:
-            temporizador = fuente_reloj.render(f"{tiempo_restante}", True, colores.ROJO)
-        ventana.blit(temporizador, (240, 640))
-    
+            tiempo_restante = 0  
+            color = colores.ROJO
+        tiempo_restante_str = str(tiempo_restante).zfill(2)
+        temporizador = fuente_reloj.render(tiempo_restante_str, True, color)
+        
+        ventana.blit(temporizador, (237, 637))
+
     pygame.display.update()  # actualiza la pantalla
     tiempo.tick(FPS)
 
